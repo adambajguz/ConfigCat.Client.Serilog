2cd4e8cbd66e9e8fcee0dd8ff75a5f3ecaba6b94
Adam Bajguz <adam.bajguz@gmail.com>
2019-09-17 19:38:37 +0200

    Code

---
+++ ConfigCat.Client.Serilog/SerilogLogger.cs
@@ -0,0 +1,42 @@
+﻿namespace ConfigCat.Client.Serilog
+{
+    public class SerilogLogger : ILogger
+    {
+        private readonly string _LoggerName;
+        private readonly LogLevel _MinLogLevel;
+
+        public SerilogLogger(string loggerName, LogLevel minLogLevel)
+        {
+            _LoggerName = loggerName;
+            _MinLogLevel = minLogLevel;
+        }
+
+        public void Debug(string message)
+        {
+            if (TargetLogEnabled(_MinLogLevel))
+                global::Serilog.Log.Debug(FormatMessage(LogLevel.Debug, message));
+        }
+
+        public void Information(string message)
+        {
+            if (TargetLogEnabled(_MinLogLevel))
+                global::Serilog.Log.Information(FormatMessage(LogLevel.Info, message));
+        }
+
+        public void Warning(string message)
+        {
+            if (TargetLogEnabled(_MinLogLevel))
+                global::Serilog.Log.Warning(FormatMessage(LogLevel.Warn, message));
+        }
+
+        public void Error(string message)
+        {
+            if (TargetLogEnabled(_MinLogLevel))
+                global::Serilog.Log.Error(FormatMessage(LogLevel.Error, message));
+        }
+
+        private bool TargetLogEnabled(LogLevel targetTrace) => (byte)targetTrace <= (byte)_MinLogLevel;
+
+        protected virtual string FormatMessage(LogLevel logLevel, string message) => $"{_LoggerName ?? ""} - {message}";
+    }
+}
---
+++ ConfigCat.Client.Serilog/SerilogLoggerFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace ConfigCat.Client.Serilog
+{
+    public class SerilogLoggerFactory : ILoggerFactory
+    {
+        private readonly LogLevel minLogLevel;
+
+        public SerilogLoggerFactory(LogLevel minLogLevel)
+        {
+            this.minLogLevel = minLogLevel;
+        }
+
+        public ILogger GetLogger(string loggerName)
+        {
+            return new SerilogLogger(loggerName, this.minLogLevel);
+        }
+    }
+}
